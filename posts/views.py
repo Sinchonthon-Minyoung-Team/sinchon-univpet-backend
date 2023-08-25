2a1145819223e8a8763181f353e08e41d209d1d6
Jiseok Son <jiseokson@gmail.com>
2023-08-25 22:24:30 +0900

    add: posts view set

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from posts.models import Posts
+from posts.serializers import PostSerializer
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    # permission_classes = (IsAuthenticated,)
+    
+    def get_queryset(self):
+        return Posts.objects.order_by('-created_at')
