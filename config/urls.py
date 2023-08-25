9107845aa5d9c9590c8afc1af40f716e63e4d0c8
kyubin0314@naver.com <kyubin0314@naver.com>
2023-08-26 00:18:33 +0900

    :sparkles: d-day 정렬 추가

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, include, path
+from django.urls import include, path
+from posts.views import PostViewSet
+from posts.views import SortedPostListView  # 해당 뷰를 import 해야합니다.
 
 from rest_framework_simplejwt.views import TokenRefreshView
-
 from rest_framework.routers import DefaultRouter
 
 from accounts.views import AccountCreateRetrieveViewSet
@@ -27,24 +12,14 @@ from auths.views import OAuthTokenObtainView
 accounts_router = DefaultRouter()
 accounts_router.register(r'accounts', AccountCreateRetrieveViewSet, basename='accounts')
 
-from rest_framework.routers import DefaultRouter
-
-from posts.views import PostViewSet
-
 posts_router = DefaultRouter()
 posts_router.register(r'posts', PostViewSet, basename='posts')
 
-accounts_router = DefaultRouter()
-accounts_router.register(r'accounts', AccountCreateRetrieveViewSet, basename='accounts')
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('auth/<str:provider>/token',
-         OAuthTokenObtainView.as_view(), name='token_obtain'),
+    path('auth/<str:provider>/token', OAuthTokenObtainView.as_view(), name='token_obtain'),
     path('auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('', include(accounts_router.urls)),
-
+    path('sorted_posts/', SortedPostListView.as_view(), name='sorted-posts'),  # d-day로 정렬된 리스트 불러옴
     path('', include(posts_router.urls)),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
@@ -6,12 +7,31 @@ from rest_framework.permissions import IsAuthenticated
 from posts.models import Posts
 from posts.serializers import PostSerializer
 
+from django.db.models import DateTimeField, ExpressionWrapper, F
+from django.db.models.functions import Now
+from rest_framework.generics import ListAPIView
+
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(writer=self.request.user)
     
     def get_queryset(self):
         return Posts.objects.order_by('-created_at')
+    
+class SortedPostListView(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        queryset = Posts.objects.all()
+        queryset = queryset.annotate(
+            expire_at=ExpressionWrapper(
+                F('created_at') + timedelta(days=1) * F('duration'),
+                output_field=DateTimeField()
+            )
+        )
+        
+        queryset = sorted(queryset, key=lambda x: x.expire_at)  # 만료 시간을 기준으로 정렬
+        return queryset
