79d365111bb3d7ca5c199dc6646694cf42e015cf
fressh1127 <fressh1127@gmail.com>
2023-08-25 23:36:35 +0900

    post view url 수정

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
@@ -6,12 +7,23 @@ from rest_framework.permissions import IsAuthenticated
 from posts.models import Posts
 from posts.serializers import PostSerializer
 
+from django.http import HttpResponse
+
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated,)
+    #permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer(writer=self.request.user).save()
     
     def get_queryset(self):
         return Posts.objects.order_by('-created_at')
+    
+def post_detail(request, post_id):
+    try:
+        post = Posts.objects.get(pk=post_id)
+        response_text = f"게시글 ID: {post.id}, 제목: {post.title}, 내용: {post.content}, 좋아요: {post.likes}"
+        return HttpResponse(response_text)
+    except Posts.DoesNotExist:
+        return HttpResponse("해당 게시글을 찾을 수 없습니다.", status=404)
+    
