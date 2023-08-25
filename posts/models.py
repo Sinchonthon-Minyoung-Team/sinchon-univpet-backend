e78753989a4cb8609a672c67d318a70dcbb7bf74
soobin <shinsoobin1111@gmail.com>
2023-08-25 22:00:48 +0900

    posts 1st

---
+++ posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Posts(models.Model):
+    CATEGORY_CHOICES = (
+        ('MO', 'Morestudents'),
+        ('CU', 'Curriculum'),
+        ('PR', 'Professor'),
+        ('ET', 'Etc'),
+    )
+    STATUS_CHOICES = (
+        ('CU', 'Current'),
+        ('EN', 'End'),
+    )
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    purpose = models.TextField()
+    content = models.TextField()
+    likes = models.IntegerField()
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField()
